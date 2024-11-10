5b5eeeb8f9efde35fae88c6317bf257e11cdc1f9
Max Modlin <max.modlin@gmail.com>
2024-11-10 14:02:03 +0000

    Commits 2

--- app/styling.py
+++ app/styling.py
@@ -1,13 +1,16 @@
 import streamlit as st
+import os
 
 def template1_page_style():  
-    with open('../templates/template1_style.css') as f:
-    #with open('C:\\Users\\Administrator\\theah-mvp\\templates\\template1_style.css') as f:
+    script_dir = os.path.dirname(__file__)
+    css_path = os.path.join(script_dir, '../templates/template1_style.css')
+    with open(css_path) as f:
         css = f.read()
-    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+        st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
 def template2_page_style():  
-    with open('../templates/template2_style.css') as f:
-    #with open('C:\\Users\\Administrator\\theah-mvp\\templates\\template2_style.css') as f:
+    script_dir = os.path.dirname(__file__)
+    css_path = os.path.join(script_dir, '../templates/template2_style.css')
+    with open(css_path) as f:
         css = f.read()
     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
